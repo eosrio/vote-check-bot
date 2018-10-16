49488c54f0ee6038d80dda2c193f51b9b997283a
joaoocb <joaoocb@gmail.com>
2018-10-15 21:06:45 -0300

    Updating bot messages.

---
+++ bot_strings.js
@@ -0,0 +1,23 @@
+module.exports = {
+    ACCOUNT_REGISTERED: "Account registered! I'll send you a message when it is time to confirm your votes.",
+    ACCOUNT_REMOVED: "Account removed!",
+    ALREADY_REGISTERED: "Account already registered!",
+    ACCOUNT_NOT_FOUND: "Account not found!",
+    REGISTER_ACCOUNT: "To register an account type: /register ACCOUT_NAME THRESHOLD.",
+    REMOVE_ACCOUNT: "To remove an account type: /remove ACCOUNT_NAME",
+    MORE_INFORMATION: "For more information on how to use me type: /help.",
+    WARNING_MESSAGE: "Hey there! It is time to confirm your votes!",
+    VOTING_UPDATED: "Voting power updated, I will notify you when your voting power has decayed by ",
+    WARNING_TIME: "It should happen on ",
+    WELCOME: "Welcome!\n\nDid you know that as time passes your voting power on the EOS platform decays and " +
+        "that you should update it once in a while? \n\nI can help you keep track of your votes, " +
+        "all you need to do is register your EOS account and I'll let you know when the time to confirm them comes.",
+    HELP: "I can help you keep track of your votes on the EOS platform. You can perform the following actions:\n\n" +
+        "*Register an EOS account*\n\n" +
+        "    Usage: /register ACCOUNT THRESHOLD\n\n" +
+        "    THRESHOLD represents the % of voting power you accept to loose.\n\n" +
+        "    Example: /register myaccount123 20\n\n" +
+        "*Remove an EOS account*\n\n" +
+        "    Usage: /remove ACCOUNT\n\n" +
+        "    Example: /remove myaccount123"
+};
--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const Promise = require("bluebird");
 // EOSJS
 const eosjs = require("eosjs");
 const fetch = require("node-fetch");
-// const {TextDecoder, TextEncoder} = require("text-encoding");
 const rpc = new eosjs.Rpc.JsonRpc(config.bp.api_url, {fetch});
 
 // Telegram
@@ -12,28 +11,48 @@ const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(config.telegram_bot.bot_token, {polling: true});
 
 // Assets
+const assert = require('assert');
 const util = require("./util");
+const strings = require("./bot_strings");
 
 // Mongodb
 const MongoClient = require("mongodb").MongoClient;
-const assert = require('assert');
+const client = new MongoClient(config.mongodb.db_url, {useNewUrlParser: true});
 let accounts;
-const client = new MongoClient(config.mongodb.db_url, {
-    useNewUrlParser: true
-});
 
 client.connect(function (err) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
+
     const db = client.db(config.mongodb.db_name);
     accounts = db.collection("accounts");
+
     start();
 });
 
-function check_account(doc) {
+function start() {
+    setInterval(() => {
+        main();
+    }, config.sleep_time)
+}
 
+function main() {
+    check_status();
+}
+
+function check_status() {
+    accounts.find({}).toArray((err, result) => {
+        if (!err) {
+            if (result) {
+                Promise.map(result, check_account, {concurrency: 1});
+            }
+        }
+    });
+}
+
+function check_account(doc) {
     const account = doc.account;
-    console.log(account);
+    console.log("Checking account: ", account);
 
     return new Promise((resolve) => {
         rpc.get_account(account).then((data) => {
@@ -63,31 +82,23 @@ function check_account(doc) {
                     $set: {last_weight: past_weight, alerted: false}
                 }, {upsert: false}).then(() => {
                     const futureDate = util.calcTime(past_weight, doc.limit, data.voter_info.staked);
-                    bot.sendMessage(doc.chat_id, "Voting power updated, I will notify you when your voting power has decayed by " + doc.limit + "% \n Should be around " + futureDate);
+                    const message = strings.VOTING_UPDATED + doc.limit + "%\n\n" + strings.WARNING_TIME + futureDate;
+                    bot.sendMessage(doc.chat_id, message);
                     resolve();
                 }).catch((err) => {
                     resolve(err);
                 });
             }
         }).catch((err) => {
-            console.log("Error requesting data from ", config.bp.api_url, err);
+            console.log("Error requesting data from ", config.bp.api_url, "\n", err);
             resolve(err);
         });
     });
 }
 
-function check_status() {
-    accounts.find({}).toArray((err, result) => {
-        if (!err) {
-            if (result) {
-                Promise.map(result, check_account, {concurrency: 1});
-            }
-        }
-    });
-}
-
 function register_account(username, account, limit, chat_id) {
     console.log('Registering account: ' + account);
+
     return new Promise((resolve, reject) => {
         rpc.get_account(account).then((result) => {
             const past_weight = parseFloat(result['voter_info']['last_vote_weight']);
@@ -121,23 +132,25 @@ function remove_account(username, account) {
 }
 
 function send_warning(chat_id) {
-    bot.sendMessage(chat_id, "Hey there! It is time for you to confirm your votes!");
+    bot.sendMessage(chat_id, strings.WARNING_MESSAGE);
 }
 
 bot.onText(/\/register/, (msg) => {
     const params = msg.text.toString().split(" ");
     const account = params[1];
-    const limit = Number(params[2]);
-    if (params.length === 3 && util.validate_account(account) && util.validate_threshold(limit)) {
-        register_account(msg.from.username, account, limit, msg.chat.id).then((futureDate) => {
-            bot.sendMessage(msg.chat.id, "Account registered! I'll send you a message when " +
-                "it is time to confirm your votes. Should be around " + futureDate);
+    const threshold = Number(params[2]);
+
+    if (params.length === 3 && util.validate_account(account) && util.validate_threshold(threshold)) {
+        register_account(msg.from.username, account, threshold, msg.chat.id).then((futureDate) => {
+            const message = strings.ACCOUNT_REGISTERED + "\n\n" + strings.WARNING_TIME + futureDate;
+            bot.sendMessage(msg.chat.id, message);
         }).catch((e) => {
             console.log(e);
-            bot.sendMessage(msg.chat.id, "Account already registered!");
+            bot.sendMessage(msg.chat.id, strings.ALREADY_REGISTERED);
         });
     } else {
-        bot.sendMessage(msg.chat.id, "To register an account type: /register ACCOUNT_NAME LIMIT");
+        const message = strings.REGISTER_ACCOUNT + "\n\n" + strings.MORE_INFORMATION;
+        bot.sendMessage(msg.chat.id, message);
     }
 });
 
@@ -147,38 +160,28 @@ bot.onText(/\/remove/, (msg) => {
         const account = params[1];
         remove_account(msg.from.username, account).then((result) => {
             if (result.result.n === 1) {
-                bot.sendMessage(msg.chat.id, "Account removed!");
+                bot.sendMessage(msg.chat.id, strings.ACCOUNT_REMOVED);
             } else {
-                bot.sendMessage(msg.chat.id, "Account not found!");
+                bot.sendMessage(msg.chat.id, strings.ACCOUNT_NOT_FOUND);
             }
         });
     } else {
-        bot.sendMessage(msg.chat.id, "To remove an account type: /remove ACCOUNT_NAME");
+        const message = strings.REMOVE_ACCOUNT + "\n\n" + strings.MORE_INFORMATION;
+        bot.sendMessage(msg.chat.id, message);
     }
 });
 
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Welcome! I can help you keep track of your votes on EOS platform! " +
-        "As the time passes, your voting power decays and it is importante to confirm " +
-        "them once in a while. All you need to do is register your EOS account and I'll " +
-        "let you know when the time comes.");
+    const message = strings.WELCOME + "\n\n" + strings.MORE_INFORMATION;
+    bot.sendMessage(msg.chat.id, message);
 });
 
 bot.onText(/\/help/, (msg) => {
-    bot.sendMessage(msg.chat.id, "To register an account type: /register ACCOUNT_NAME LIMIT " +
-        "where 0 < LIMIT < 100. LIMIT represents the % of voting power you accept to loose.");
-    bot.sendMessage(msg.chat.id, "To remove an account type: /remove ACCOUNT_NAME");
+    bot.sendMessage(msg.chat.id, strings.HELP, {parse_mode : "Markdown"});
 });
 
-function main() {
-    check_status();
-}
-
-function start() {
-    main();
-    setInterval(() => {
-        main();
-    }, config.sleep_time)
-}
-
-
+bot.on('message', (msg) => {
+    if(msg.text.toString().search(/(\/register|\/remove|\/start|\/help)/) === -1) {
+        bot.sendMessage(msg.chat.id, strings.MORE_INFORMATION);
+    }
+});
